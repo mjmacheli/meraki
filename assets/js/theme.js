b644399925c44c9707d4e22fabdde8db2ff4a3ee
MJ Macheli <machelimail@gmail.com>
2021-06-30 03:02:27 +0200

    updated

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -33,24 +33,6 @@
         };
     };
     
-    //*  Main slider js 
-    // function home_main_slider(){
-    //     if ( $('.slider_inner').length ){
-    //         $('.slider_inner').camera({
-    //             loader: true,
-    //             navigation: true,
-    //             autoPlay:false,
-    //             time: 4000,
-    //             playPause: false,
-    //             pagination: false,
-    //             thumbnails: false,
-    //             overlayer: true,
-    //             hover: false,  
-    //             minHeight: '500px',
-    //         }); 
-    //     }
-    // }
-    
     //* Isotope Js
     function portfolio_isotope(){
         if ( $('.portfolio_item, .portfolio_2 .portfolio_filter ul li').length ){
